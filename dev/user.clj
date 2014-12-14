de5f8f2b543e2271979f0a9a6d670d7f45a2d419
Dmytro Budnyk <dmitriy.budnik@gmail.com>
2014-12-15 01:00:51 +0200

    wip3

--- dev/user.clj
+++ dev/user.clj
@@ -1,15 +1,18 @@
 (ns user
+  (:use [org.httpkit.server :only [run-server]])
   (:require [leiningen.core.main :as lein]
             [clojure.java.io :as io]
             [compojure.core :refer [GET POST defroutes]]
             [compojure.route :refer [resources]]
             [compojure.handler :refer [api]]
             [ring.middleware.reload :as reload]
+            [ring.middleware.defaults]
             [environ.core :refer [env]]
-            [taoensso.sente :as sente]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [taoensso.sente :as sente]))
 
-(let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]} (sente/make-channel-socket! {})]
+(defonce sente-socket (sente/make-channel-socket! {}))
+
+(let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]} sente-socket]
   (def ring-ajax-post                ajax-post-fn)
   (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
   (def ch-chsk                       ch-recv) ; ChannelSocket's receive channel
@@ -23,39 +26,52 @@
   (POST "/chsk" req (ring-ajax-post                req))
   (GET "/*" req (io/resource "index.html")))
 
+(defn- event-msg-handler
+  [{:as ev-msg :keys [ring-req event ?reply-fn]} _]
+  (let [session (:session ring-req)
+        uid     (:uid session)
+        [id data :as ev] event]
+    (printf "Event: %s (UID: %s)" ev uid)))
 
-(def my-ring-handler
+(defonce chsk-router
+    (sente/start-chsk-router-loop! event-msg-handler ch-chsk))
+
+(def http-handler
   (let [ring-defaults-config
         (assoc-in ring.middleware.defaults/site-defaults [:security :anti-forgery]
           {:read-token (fn [req] (-> req :params :csrf-token))})]
-   (ring.middleware.defaults/wrap-defaults my-routes ring-defaults-config)))
 
-(defmulti event-msg-handler :id) ; Dispatch on event-id
-;; Wrap for logging, catching, etc.:
-(defn     event-msg-handler* [{:as ev-msg :keys [id ?data event]}]
-  (logf "Event: %s" event)
-  (event-msg-handler ev-msg))
+    ;; NB: Sente requires the Ring `wrap-params` + `wrap-keyword-params`
+    ;; middleware to work. These are included with
+    ;; `ring.middleware.defaults/wrap-defaults` - but you'll need to ensure
+    ;; that they're included yourself if you're not using `wrap-defaults`.
+    ;;
+    (ring.middleware.defaults/wrap-defaults routes ring-defaults-config)))
 
-#+clj
-(do ; Server-side methods
-  (defmethod event-msg-handler :default ; Fallback
-    [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
-    (let [session (:session ring-req)
-          uid     (:uid     session)]
-      (logf "Unhandled event: %s" event)
-      (when ?reply-fn
-        (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
 
-  ;; Add your (defmethod event-msg-handler <event-id> [ev-msg] <body>)s here...
-  )
+; (defmulti event-msg-handler :id) ; Dispatch on event-id
+; ;; Wrap for logging, catching, etc.:
+; (defn     event-msg-handler* [{:as ev-msg :keys [id ?data event]}]
+;   (print event)
+;   (event-msg-handler ev-msg))
+; (do ; Server-side methods
+;   (defmethod event-msg-handler :default
+;     [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
+;     (let [session (:session ring-req)
+;           uid     (:uid     session)]
+;       (print (str "Unhandled event: " event))
+;       (when ?reply-fn
+;         (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
 
-(def http-handler (reload/wrap-reload (api #'routes)))
+;   ;; Add your (defmethod event-msg-handler <event-id> [ev-msg] <body>)s here...
+;   )
+
 
 (defn run [& [port]]
   (defonce ^:private server
     (let [port (Integer. (or port (env :port) 10555))]
-      (print "Starting web server on port" port ".\n")
-      (run-jetty http-handler {:port port
+      (print "Starting web server on port " port)
+      (run-server (reload/wrap-reload (var http-handler)) {:port port
                                :join? false})))
   server)
 
--- src/foo/core.cljs
+++ src/foo/core.cljs
@@ -2,26 +2,25 @@
   (:require-macros [cljs.core.async.macros :as asyncm :refer (go go-loop)])
   (:require [reagent.core :as reagent :refer [atom]]
             [clojure.string :as string]
-            [cljs.core.async :as async :refer (<! >! put! chan)]
+            [cljs.core.async :as async :refer (<! >! put! chan timeout)]
             [taoensso.sente  :as sente :refer (cb-success?)]))
 
-(defonce app-state (atom {:text "Hello, what is your name? "}))
+(defonce app-state (atom {:text "Hello, what is your name?!"}))
 
 (def oauth-url
   (apply str
     (string/split
-        "https://accounts.google.com/o/oauth2/auth?
-        scope=email%20profile&
-        state=%2Fprofile&
-        redirect_uri=http%3A%2F%2Flocalhost%3A10555%2Foauth2callback&
-        response_type=token&
-        client_id=722505593932-g5ha7dlvljr5e0bp4l8hqmmdc5s4pumu.apps.googleusercontent.com"
-         #"\s+")
-))
+      "https://accounts.google.com/o/oauth2/auth?
+      scope=email%20profile&
+      state=%2Fprofile&
+      redirect_uri=http%3A%2F%2Flocalhost%3A10555%2Foauth2callback&
+      response_type=token&
+      client_id=722505593932-g5ha7dlvljr5e0bp4l8hqmmdc5s4pumu.apps.googleusercontent.com"
+       #"\s+")))
 
 (def token_from_url
   (peek 
-    (re-find #"(?:access_token=)([^&]+)" js/document.location.hash))))
+    (re-find #"(?:access_token=)([^&]+)" js/document.location.hash)))
 
 (defn page []
   [:div (@app-state :text) "Kotala la-la-la"
@@ -31,7 +30,7 @@
 
 (let [{:keys [chsk ch-recv send-fn state]}
       (sente/make-channel-socket! "/chsk" ; Note the same path as before
-       {:type :auto ; e/o #{:auto :ajax :ws}
+       {:type :ws ; e/o #{:auto :ajax :ws}
        })]
   (def chsk       chsk)
   (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
@@ -40,4 +39,12 @@
   )
 
 (defn main []
-  (reagent/render-component [page] (.getElementById js/document "app")))
+  (reagent/render-component [page] (.getElementById js/document "app"))
+  (when-let [token token_from_url]
+    (chsk-send! [:login/happend token])
+    ; (go-loop [seconds 1]
+    ;           (<! (timeout 1000))
+             
+    ;           (print "waited" seconds "seconds")
+    ;           (recur (inc seconds))
+    ))
